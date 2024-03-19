6c23d0113513ab69b166617c8d16c2df7f15c53b
Sebastian Villada Jaramillo <sebastian.villada.jaramillo@gmail.com.co>
2024-03-18 22:25:50 -0500

    [MODIFY] component TodoSearch

--- src/App.js
+++ src/App.js
@@ -3,32 +3,38 @@ import { TodoSearch } from "./components/TodoSearch/TodoSearch";
 import { TodoList } from "./components/TodoList/TodoList";
 import { TodoItem } from "./components/TodoItem/TodoItem";
 import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton";
-
+import React from "react";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
   { text: "Tomar el curso de Intro a React.js", completed: false },
   { text: "Llorar con la llorona", completed: true },
   { text: "Comer hamburguesa", completed: false },
-  { text: "Comer hamburguesa", completed: true },
-  { text: "Comer hamburguesa", completed: false },
-  { text: "Comer hamburguesa", completed: true },
-  { text: "Comer hamburguesa", completed: false }
+  { text: "Comer hamburguesa", completed: true }
 ];
 
 //Componente de React
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  console.log("Los usuarios buscan todos de " + searchValue);
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
   return (
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {defaultTodos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
+        {defaultTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
             text={todo.text}
-            completed={todo.completed} 
+            completed={todo.completed}
           />
         ))}
         {[]}
