1a1c2fee4ccb9bc2b1177838b7569e374a1a636d
Sebastian Villada Jaramillo <sebastian.villada.jaramillo@gmail.com.co>
2024-03-19 00:09:27 -0500

    [MODIFY] components app.js and TodoItem.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import React from "react";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
-  { text: "Tomar el curso de Intro a React.js", completed: false },
-  { text: "Llorar con la llorona", completed: true },
-  { text: "Comer hamburguesa", completed: false },
-  { text: "Comer hamburguesa", completed: true }
+  { text: "Tomar el Curso de Intro a React.js", completed: false },
+  { text: "Llorar con la Llorona", completed: false },
+  { text: "LALALALALA", completed: false },
+  { text: "Usar estados derivados", completed: true },
 ];
 
 //Componente de React
@@ -18,26 +18,44 @@ function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
   const [searchValue, setSearchValue] = React.useState("");
 
-  console.log("Los usuarios buscan todos de " + searchValue);
-
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos} />
-
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {defaultTodos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
-        {[]}
       </TodoList>
 
       <CreateTodoButton />
